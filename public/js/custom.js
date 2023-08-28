7e551b51b4fb6f6477620bd1099c3555161ab4b7
sachinsinghcode <sachinsingh.code@outlook.com>
2023-08-28 08:43:00 +0530

    Changed Text and Map location

--- public/js/custom.js
+++ public/js/custom.js
@@ -10,7 +10,7 @@ getYear();
 /** google_map js **/
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(28.641485, 77.371384),
+        center: new google.maps.LatLng(28.622169, 77.391510),
         zoom: 15,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
